212d222f161939f2c31b3c6d85a35568de2a3065
周东闯 <zhoudongchuang@51shebao.com>
2018-07-27 14:42:08 +0800

    name

--- test/UpploadFileTest.php
+++ test/UpploadFileTest.php
@@ -2,7 +2,7 @@
 
 require_once '../vendor/autoload.php';
 
-use Guanguans\UploadFile;
+use Zhoudongchuang\UploadFile;
 
 $upload = new UploadFile();
 $upload->maxSize       = 1*1024*1024;    // 默认为-1，不限制上传大小
